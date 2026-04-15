e31ed083141239f71b42fed9e2ea546de03067ec
Zaid Shahid <144483692+ZaidShahid15@users.noreply.github.com>
2026-04-16 04:51:13 +0500

    change

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -178,7 +178,7 @@
 			<div class="elementor-widget-wrap elementor-element-populated">
 						<div class="elementor-element elementor-element-894eb01 elementor-widget elementor-widget-heading" data-id="894eb01" data-element_type="widget" data-e-type="widget" data-widget_type="heading.default">
 				<div class="elementor-widget-container">
-					<h2 class="elementor-heading-title elementor-size-default">Unser Standort
+					<h2 class="elementor-heading-title elementor-size-default">Adresse (rechtlicher Betreiber):
 </h2>				</div>
 				</div>
 				<div class="elementor-element elementor-element-1fcc6f22 elementor-icon-list--layout-traditional elementor-list-item-link-full_width elementor-widget elementor-widget-icon-list" data-id="1fcc6f22" data-element_type="widget" data-e-type="widget" data-widget_type="icon-list.default">
@@ -187,14 +187,14 @@
 							<li class="elementor-icon-list-item">
 											<span class="elementor-icon-list-icon">
 							<svg aria-hidden="true" class="e-font-icon-svg e-fas-map-marker-alt" viewBox="0 0 384 512" xmlns="http://www.w3.org/2000/svg"><path d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0zM192 272c44.183 0 80-35.817 80-80s-35.817-80-80-80-80 35.817-80 80 35.817 80 80 80z"></path></svg>						</span>
-										<span class="elementor-icon-list-text">Postfach - 115037, Dubai, V.A.E.</span>
+										<span class="elementor-icon-list-text">SchillerstraÃŸe 19/9/1, 2351 Wiener Neudorf</span>
 									</li>
 						</ul>
 						</div>
 				</div>
 				<div class="elementor-element elementor-element-2f6cc402 elementor-widget elementor-widget-heading" data-id="2f6cc402" data-element_type="widget" data-e-type="widget" data-widget_type="heading.default">
 				<div class="elementor-widget-container">
-					<h2 class="elementor-heading-title elementor-size-default">Schnellkontakt
+					<h2 class="elementor-heading-title elementor-size-default">Kontakt Eco Clean:
 </h2>				</div>
 				</div>
 				<div class="elementor-element elementor-element-3524c967 elementor-icon-list--layout-traditional elementor-list-item-link-full_width elementor-widget elementor-widget-icon-list" data-id="3524c967" data-element_type="widget" data-e-type="widget" data-widget_type="icon-list.default">
@@ -203,12 +203,12 @@
 							<li class="elementor-icon-list-item">
 											<span class="elementor-icon-list-icon">
 							<svg aria-hidden="true" class="e-font-icon-svg e-fas-phone-alt" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"></path></svg>						</span>
-										<span class="elementor-icon-list-text">+971 56 420 67 99</span>
+										<span class="elementor-icon-list-text">+43 1 442 0281</span>
 									</li>
 								<li class="elementor-icon-list-item">
 											<span class="elementor-icon-list-icon">
 							<svg aria-hidden="true" class="e-font-icon-svg e-fas-envelope" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M502.3 190.8c3.9-3.1 9.7-.2 9.7 4.7V400c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V195.6c0-5 5.7-7.8 9.7-4.7 22.4 17.4 52.1 39.5 154.1 113.6 21.1 15.4 56.7 47.8 92.2 47.6 35.7.3 72-32.8 92.3-47.6 102-74.1 131.6-96.3 154-113.7zM256 320c23.2.4 56.6-29.2 73.4-41.4 132.7-96.3 142.8-104.7 173.4-128.7 5.8-4.5 9.2-11.5 9.2-18.9v-19c0-26.5-21.5-48-48-48H48C21.5 64 0 85.5 0 112v19c0 7.4 3.4 14.3 9.2 18.9 30.6 23.9 40.7 32.4 173.4 128.7 16.8 12.2 50.2 41.8 73.4 41.4z"></path></svg>						</span>
-										<span class="elementor-icon-list-text">project.coordinator@arrowhead.ae</span>
+										<span class="elementor-icon-list-text">office@eco-clean.at</span>
 									</li>
 						</ul>
 						</div>
